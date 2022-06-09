9b894071b02ee6b8e54d06f6eac91ce07fbb14f0
Vinicius Giacomelli <giacomelli.vinicius@gmail.com>
2022-06-09 09:49:39 -0300

    :sparkles: feat: Services Implementation and Services Exception
    That modification add a implementation of the UserService and
    create a ObjectNotFoundClass

--- src/main/java/com/vinicius/api/services/implementations/UserServiceImpl.java
+++ src/main/java/com/vinicius/api/services/implementations/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.vinicius.api.services.implementations;
 
 import com.vinicius.api.domain.User;
 import com.vinicius.api.repositories.UserRepository;
+import com.vinicius.api.services.exceptions.ObjectNotFoundException;
 import com.vinicius.api.services.interfaces.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,6 @@ public class UserServiceImpl implements UserServices {
     @Override
     public User findById(Integer id) {
         Optional<User> user = repository.findById(id);
-        return user.orElse(null);
+        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
